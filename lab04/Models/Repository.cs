9a9100f55ca58a7cffcff9ce539e80156a0cb35d
Oberdan <oberdan.bitencourt@gmail.com>
2017-03-05 17:37:59 -0300

    lab4

---
+++ lab04/Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MaratonaXamarin
+{
+    public class Repository
+    {
+        public async Task<List<Cat>> GetCats()
+        {
+            List<Cat> cats;
+            var webApiUrl = "http://demos.ticapacitacion.com/cats";
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                var json = await client.GetStringAsync(webApiUrl);
+                cats = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Cat>>(json);
+            }
+            return cats;
+        }
+    }
+}
